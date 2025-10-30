8fb4b621bda92c8fe914fd3d9337d93a766d86d6
Mary Stone <mary@Marys-MacBook-Pro.local>
2025-10-30 07:52:43 -0400

    changes to use goloang microservices

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,44 +1,28 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { data, Link } from "react-router-dom";
 
 const Movies = () => {
 
     const [movies, setMovies] = useState([])
 
     useEffect( () => {
-        let moviesList = [
-            {
-                id: 1,
-                title: "Highlander",
-                release_date: "1986-03-07",
-                runtime: 116,
-                mpaa_rating: "R",
-                description: "Some Long Description here",
-            },{
-                id: 2,
-                title: "Raiders of the Lost Ark",
-                release_date: "1981-06-12",
-                runtime: 115,
-                mpaa_rating: "PG-13",
-                description: "Some Long Description here",
-            },{
-                id: 3,
-                title: "Jaws",
-                release_date: "1975-06-012",
-                runtime: 115,
-                mpaa_rating: "PG",
-                description: "Some Long Description here",
-            },{
-                id: 4,
-                title: "Star Wars",
-                release_date: "1979-07-13",
-                runtime: 115,
-                mpaa_rating: "PG",
-                description: "Some Long Description here",
-            }
-        ];
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
 
-        setMovies(moviesList);
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        }
+
+        fetch(`/movies`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
     }, []);
 
     return(
